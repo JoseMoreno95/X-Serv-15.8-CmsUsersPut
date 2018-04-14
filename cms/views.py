4664aad491cf97cb35a144f9df95f1a246eb18e2
José Moreno Rodríguez <josemoreno95@outlook.com>
2018-04-14 16:11:46 +0200

    Entrega de la práctica 15.8

---
+++ cms/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+
+def mainPage(request):
+    group = Pages.objects.all()
+    list = ''
+    for item in group:
+        list = list + item.name + '<br>'
+    if request.user.is_authenticated():
+        message = "Logueado como " + str(request.user) + ". " + "<a href=http://localhost:8000/logout/>Logout</a><br><br>"
+    else:
+        message = "Usuario no logueado. " + "<a href=http://localhost:8000/authenticate/>Login</a><br><br>"
+    return HttpResponse(message +
+    "La lista de recursos disponibles es la siguiente:<br><br>" +
+    list + '<br>'
+    "Un ejemplo de cómo pedir un recurso es: "
+    "http://localhost:8000/pepito<br><br>"
+    "Si pides un recurso que no existe, la página te lo indicará")
+
+@csrf_exempt
+def getPage(request, text):
+    if request.user.is_authenticated():
+        message = "Logueado como " + str(request.user) + ". " + "<a href=http://localhost:8000/logout/>Logout</a><br><br>"
+    else:
+        message = "Usuario no logueado. " + "<a href=http://localhost:8000/authenticate/>Login</a><br><br>"
+    if request.method == "GET":
+        try:
+            object = Pages.objects.get(name = text)
+            return HttpResponse(object.page)
+        except Pages.DoesNotExist:
+            return HttpResponse(message + "No hay una página para " + text)
+    else:
+        if request.user.is_authenticated():
+            page = Pages(name = text, page = request.body.decode("utf-8"))
+            page.save()
+            return HttpResponse(message + "Nueva página creada")
+        else:
+            return HttpResponse(message + "Para crear una página primero debes loguearte.")
+
+
+def loginPage(request):
+    return HttpResponse("""<form action='/login/' method='post'>
+    Usuario:<br>
+    <input type='text' name='Usuario' value=''>
+    <br>
+    Contraseña:<br>
+    <input type='password' name='Contraseña' value=''>
+    <br><br>
+    <input type='submit' value='Login'>
+    </form>""")
+
+@csrf_exempt
+def loginView(request):
+    username = request.POST['Usuario']
+    password = request.POST['Contraseña']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponse("<meta http-equiv='refresh' content='3;url=http://localhost:8000/'>"
+        "Login realizado correctamente, redirigiéndote a la página principal...")
+    else:
+        return HttpResponse("<meta http-equiv='refresh' content='3;url=http://localhost:8000/authenticate/'>"
+        "Login erróneo, redirigiéndote a la página de login...")
+
+def logoutView(request):
+    logout(request)
+    return HttpResponse("<meta http-equiv='refresh' content='3;url=http://localhost:8000/'>"
+    "Logout realizado correctamente, redirigiéndote a la página principal...")
